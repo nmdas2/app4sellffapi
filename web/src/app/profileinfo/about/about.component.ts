898a2f1b6c2da434e09e6326e8b33d5d4a548c80
das <mariyadasu.nukathoti@cdk.com>
2019-08-07 23:10:05 +0530

    initial commit for social links add functionality

--- web/src/app/profileinfo/about/about.component.ts
+++ web/src/app/profileinfo/about/about.component.ts
@@ -15,24 +15,24 @@ import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 export class AboutComponent implements OnInit {
   userAboutInfoList: userAboutInfo[];
   textValue: string = 'initial value';
-  isAboutInEditMode:boolean = false;
-  AllowImageUpload:boolean = false;
+  isAboutInEditMode: boolean = false;
+  AllowImageUpload: boolean = false;
   loggedInUserInfo: User;
   altrPath: string;
 
   fileData: File = null;
-  previewUrl:any = null;
+  previewUrl: any = null;
   fileUploadProgress: string = null;
   uploadedFilePath: string = null;
   dynamicImg: string = "";
   modalRef: BsModalRef;
   imgGallery = []
-  
+
   constructor(
-    private profileInfoService: ProfileinfoService, 
+    private profileInfoService: ProfileinfoService,
     private http: HttpClient,
     private modalService: BsModalService
-    ) { 
+  ) {
     this.userAboutInfoList = [];
     this.loggedInUserInfo = JSON.parse(localStorage.getItem('currentUser'));
     this.getUserAboutText();
@@ -45,122 +45,132 @@ export class AboutComponent implements OnInit {
     this.isAboutInEditMode = true;
   }
 
-  saveupdatedabout(value: string):void {
+  saveupdatedabout(value: string): void {
     this.isAboutInEditMode = false;
 
-    if(this.textValue){
+    if (this.textValue) {
       let userAboutInfoBO = <userAboutInfo>{};
       userAboutInfoBO.About = this.textValue;
       userAboutInfoBO.UserId = this.loggedInUserInfo.UserId;
       this.profileInfoService.postUserAboutText(userAboutInfoBO)
-      .subscribe(res => {
-        this.getUserAboutText();
-      }, error => {
-        console.log(error);
-      })
+        .subscribe(res => {
+          this.getUserAboutText();
+        }, error => {
+          console.log(error);
+        })
     }
   }
 
-  getUserAboutText():void{
+  getUserAboutText(): void {
     this.userAboutInfoList = [];
-    this.profileInfoService.getUsersAboutNGalleryInfo(this.loggedInUserInfo.UserId,ProfileSection.About)
-    .subscribe(res =>{
-      if(res && res.length)
-        this.userAboutInfoList = res;        
-        this.imgGallery=[];
-        for(let img of res){
-          this.altrPath=img.ImagePath;
-          if(img.Type == '2')
-          {
+    this.profileInfoService.getUsersAboutNGalleryInfo(this.loggedInUserInfo.UserId, ProfileSection.About)
+      .subscribe(res => {
+        if (res && res.length)
+          this.userAboutInfoList = res;
+        this.imgGallery = [];
+        for (let img of res) {
+          this.altrPath = img.ImagePath;
+          if (img.Type == '2') {
             img.ImagePath = "./././assets/selfprflimages/pdf_icon.png";
           }
-          let image={
-            "id":img.AutoId,
-            "imgUrl":img.ImagePath,
-            "galType":img.Type,
-            "altUrl":this.altrPath
+          let image = {
+            "id": img.AutoId,
+            "imgUrl": img.ImagePath,
+            "galType": img.Type,
+            "altUrl": this.altrPath
           };
           this.imgGallery.push(image);
         }
         this.textValue = this.userAboutInfoList[0].About;
-    }, error =>{
-      console.log(error);
-    })
+      }, error => {
+        console.log(error);
+      })
   }
 
   fileProgress(fileInput: any) {
     this.fileData = <File>fileInput.target.files[0];
     this.preview();
-}
-
-preview() {
-  // Show preview 
-  var mimeType = this.fileData.type;
-  if (mimeType.match(/image\/*/) == null) {
-    return;
   }
 
-  var reader = new FileReader();      
-  reader.readAsDataURL(this.fileData); 
-  reader.onload = (_event) => { 
-    this.previewUrl = reader.result; 
-  }
-}
- 
-onSubmit() {
-  const formData = new FormData();
-  formData.append('files', this.fileData);
-   console.log(this.fileData.name)
-  this.fileUploadProgress = '0%';
-  this.http.post('http://localhost:50517/api/ProfileInfo/SaveImagesForGallery', formData, {
-    reportProgress: true,
-    observe: 'events'   
-  })
-  .subscribe(events => {
-    if(events.type === HttpEventType.UploadProgress) {
-      this.fileUploadProgress = Math.round(events.loaded / events.total * 100) + '%';
-      console.log(this.fileUploadProgress);
-    } else if(events.type === HttpEventType.Response) {
-      this.fileUploadProgress = '';
-      console.log(events.body);
-      //alert('SUCCESS !!');
-    }       
-  })  
-  this.saveimagedocdetails(this.fileData.name);
-  this.AllowImageUpload = false;
-}
+  preview() {
+    // Show preview 
+    var mimeType = this.fileData.type;
+    if (mimeType.match(/image\/*/) == null) {
+      return;
+    }
 
-saveimagedocdetails(flname: string):void {
-  let userAboutInfoBO = <userAboutInfo>{};
+    var reader = new FileReader();
+    reader.readAsDataURL(this.fileData);
+    reader.onload = (_event) => {
+      this.previewUrl = reader.result;
+    }
+  }
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('files', this.fileData);
+    console.log(this.fileData.name)
+    this.fileUploadProgress = '0%';
+    this.http.post('http://localhost:50517/api/ProfileInfo/SaveImagesForGallery', formData, {
+      reportProgress: true,
+      observe: 'events'
+    })
+      .subscribe(events => {
+        if (events.type === HttpEventType.UploadProgress) {
+          this.fileUploadProgress = Math.round(events.loaded / events.total * 100) + '%';
+          console.log(this.fileUploadProgress);
+        } else if (events.type === HttpEventType.Response) {
+          this.fileUploadProgress = '';
+          console.log(events.body);
+          //alert('SUCCESS !!');
+        }
+      })
+    this.saveimagedocdetails(this.fileData.name);
+    this.AllowImageUpload = false;
+  }
+
+  saveimagedocdetails(flname: string): void {
+    let userAboutInfoBO = <userAboutInfo>{};
     userAboutInfoBO.About = this.textValue;
     userAboutInfoBO.UserId = this.loggedInUserInfo.UserId;
     userAboutInfoBO.Type = UploadType.Image;
     userAboutInfoBO.Section = ProfileSection.About;
-    userAboutInfoBO.ImagePath = consts.ImagesPath+flname
+    userAboutInfoBO.ImagePath = consts.ImagesPath + flname
     this.profileInfoService.postUserImagesNDocs(userAboutInfoBO)
-    .subscribe(res => {
-      this.getUserAboutText();
-    }, error => {
-      console.log(error);
-    })
-}
+      .subscribe(res => {
+        this.getUserAboutText();
+      }, error => {
+        console.log(error);
+      })
+  }
 
-checkevent(imgUrl, template, galType, altUrl)
-{
-  this.dynamicImg = imgUrl;
-  if(galType == 2){this.download(altUrl);}
-  else{this.modalRef = this.modalService.show(template);}
-}
+  checkevent(imgUrl, template, galType, altUrl) {
+    this.dynamicImg = imgUrl;
+    if (galType == 2) { this.download(altUrl); }
+    else { this.modalRef = this.modalService.show(template); }
+  }
 
-download(altUrl: string) {
+  download(altUrl: string) {
     //let blob:any = new Blob([response.blob()], { type: 'text/json; charset=utf-8' });
     //const url= window.URL.createObjectURL(blob);
     window.open(altUrl);
     //window.location.href = response.altUrl;
-}
+  }
 
-ShowGalUpPop(value: string): void {
-  this.AllowImageUpload = true;
-}
+  ShowGalUpPop(value: string): void {
+    this.AllowImageUpload = true;
+  }
 
+  //social link section
+  postLayoutType: string = "";
+  socialLink:string="";
+  showSocialLayout(type: string) {
+    this.socialLink="";
+    this.postLayoutType = type;
+  }
+  SubmitSocialLink() {
+    console.log(this.socialLink);
+    this.postLayoutType="";
+  }
+  //end social link section
 }
