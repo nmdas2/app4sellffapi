482ecdf0a8d307deb35d5d0448c5c712c7f814e6
Yashwanth Kumar Gaddam <yashwanthkumargaddam@gmail.com>
2019-09-21 19:54:54 +0530

    UI Changes -21-09-2019

--- web/src/app/constants.ts
+++ web/src/app/constants.ts
@@ -1,8 +1,8 @@
 const isProduction = false;
 export const constants = {
     AppName: "Sellff",
-    DomainURL: 'http://localhost:50517/api/',
-    //DomainURL: 'http://apollostage2.quad1test.com/practice/api/',
+    //DomainURL: 'http://localhost:50517/api/',
+    DomainURL: 'http://apollostage2.quad1test.com/practice/api/',
     //DomainURL: 'http://4sellff.com/sellffapi/api/',
     ImagesPath: 'http://localhost:50517/AppImages/',
     AboutPath: '/profileinfo/about',
