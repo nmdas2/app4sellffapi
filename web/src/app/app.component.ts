0cf28edd8f48d067c75ff9028c5c80845865ba27
LAPTOP-OGNQVK9G\vivaan <venubabu@quadone.com>
2019-11-19 09:58:25 +0530

    design chnages

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -33,7 +33,7 @@ export class AppComponent implements OnInit, OnDestroy {
     private authenticationService: AuthenticationService,
     private modalService: BsModalService,
     private commonService: CommonService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
     private _signalRService: SignalRService
   ) {
     this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
